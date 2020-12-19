1afe647285583174b32b06a50816cd551856ae04
Niels Riekert <niels.riekert@gmail.com>
2020-12-19 22:22:55 +0100

    revert back to SPA: create-react-app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+import CurrentUsage from './CurrentUsage';
+import ElectricityUsage from './ElectricityUsage';
+import GasUsage from './GasUsage';
+import ElectricityUsageMonth from './ElectricityUsageMonth';
+
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		uri: process.env.REACT_APP_API_ENDPOINT,
+	})
+});
+
+function App() {
+	return (
+		<ApolloProvider client={client}>
+			<div className="App">
+				<header className="App-header">
+					<h1>Home Care</h1>
+					<CurrentUsage />
+					<ElectricityUsage />
+					<GasUsage />
+					<ElectricityUsageMonth />
+				</header>
+			</div>
+		</ApolloProvider>
+	);
+}
+
+export default App;
