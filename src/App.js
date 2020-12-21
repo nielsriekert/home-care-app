0956e16d995ad7254ea1f0383328fe749bc9e244
Niels Riekert <niels.riekert@gmail.com>
2020-12-21 21:54:37 +0100

    #3 login form component, page and routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import './App.css';
 
-import CurrentUsage from './CurrentUsage';
-import ElectricityUsage from './ElectricityUsage';
-import GasUsage from './GasUsage';
-import ElectricityUsageMonth from './ElectricityUsageMonth';
-
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
+
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard';
 
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	link: new HttpLink({
-		uri: process.env.HOME_CARE_API_ENDPOINT,
+		uri: process.env.REACT_APP_API_ENDPOINT,
 	})
 });
 
 function App() {
 	return (
 		<ApolloProvider client={client}>
-			<div className="App">
-				<header className="App-header">
-					<h1>Home Care</h1>
-					<CurrentUsage />
-					<ElectricityUsage />
-					<GasUsage />
-					<ElectricityUsageMonth />
-				</header>
-			</div>
+			 <Router>
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/">
+						<Dashboard />
+					</Route>
+				</Switch>
+			</Router>
 		</ApolloProvider>
 	);
 }
---
+++ src/login/LoginForm.js
@@ -0,0 +1,103 @@
+import './login-form.css';
+import React, { useState, useCallback } from 'react';
+import {
+	Redirect
+} from 'react-router-dom';
+
+import { useMutation, gql } from '@apollo/client';
+
+import Cookies from 'cookies.js';
+
+import InputField from '../components/InputField';
+import Message from '../components/Message';
+import Button from '../Button';
+
+const LOGIN = gql`
+	mutation login($email: String! $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				email
+				name
+			}
+		}
+	}
+`;
+
+const getFieldValueByName = (fields, name) => {
+	const foundFields = fields.filter(field => field.name === name);
+	return foundFields.length === 1 ? foundFields[0].value : null;
+};
+
+function LoginForm() {
+	const [fields, setFields] = useState([
+		{
+			type: 'email',
+			label: 'E-mail',
+			name: 'email',
+			value: ''
+		},
+		{
+			type: 'password',
+			label: 'Password',
+			name: 'password',
+			value: ''
+		}
+	]);
+	const [error, setError] = useState(null);
+
+	const handleInputChange = useCallback((name, value) => {
+		setFields(fields.map(field => ({
+			...field,
+			value: name === field.name ? value : field.value
+		})));
+	}, [fields]);
+
+	const [login, { data }] = useMutation(LOGIN);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		login({
+			variables: {
+				email: getFieldValueByName(fields, 'email'),
+				password: getFieldValueByName(fields, 'password'),
+			}
+		}).catch(error => {
+			if (error.message !== 'Invalid email and password combination') {
+				throw error;
+			}
+
+			setError(error);
+			setFields(fields.map(field => ({
+				...field,
+				error
+			})));
+		});
+	};
+
+	if (data && data.login.token) {
+		Cookies.set('authorization-token', data.login.token, { expires: 365 });
+		return (
+			<Redirect to="/" />
+		);
+	}
+
+	return (
+		<form noValidate onSubmit={onSubmit}>
+			{fields.map(field => (
+				<InputField
+					{...field}
+					key={field.name}
+					onChange={handleInputChange}
+				/>
+			))}
+			<div className="form-footer-container">
+				{error ?
+					<Message type="error" ><p>{error.message}</p></Message> : ''}
+				<Button label="Send" type="primary" />
+			</div>
+		</form>
+	);
+}
+
+export default LoginForm;
