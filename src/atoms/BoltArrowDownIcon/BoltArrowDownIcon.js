b241bebb96e4873ae8b4d8fff7786a4f56e6a3c9
Niels Riekert <niels.riekert@gmail.com>
2021-04-14 22:52:26 +0200

    fixed BoltArrowDownIcon name

--- src/atoms/BoltArrowDownIcon/BoltArrowDownIcon.js
+++ src/atoms/BoltArrowDownIcon/BoltArrowDownIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function BoltIcon() {
+export default function BoltArrowDownIcon() {
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" width="17" height="22" viewBox="0 0 17 22"><path fill="currentColor" d="M4,22H3l1-7H.5c-.88,0-.33-.75-.31-.78Q2.13,10.8,6,4H7L6,11H9.52c.4,0,.62.19.4.66Z"/><path fill="currentColor" d="M15.38,7V5.48H12.6L17,1.08,15.92,0l-4.4,4.4V1.62H10V7Z"/></svg>
 	);
